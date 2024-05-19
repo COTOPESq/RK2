57c8945808964be418888b8a95c89f76a755b7b3
user <user@ubuntuw.myguest.virtualbox.org>
2024-05-19 12:30:04 +0300

    add cmake files

---
+++ build/CMakeFiles/ConceptualExample.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/RK2/ConceptualExample.cpp" "CMakeFiles/ConceptualExample.dir/ConceptualExample.cpp.o" "gcc" "CMakeFiles/ConceptualExample.dir/ConceptualExample.cpp.o.d"
+  "/home/user/RK2/Program.cpp" "CMakeFiles/ConceptualExample.dir/Program.cpp.o" "gcc" "CMakeFiles/ConceptualExample.dir/Program.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ConceptualExample.dir/Program.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/ConceptualExample.dir/Program.cpp.o: \
+ /home/user/RK2/Program.cpp /usr/include/stdc-predef.h
