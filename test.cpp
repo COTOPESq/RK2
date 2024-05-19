b1be04071bd532b090dcd9409bfbff735491d131
user <user@ubuntuw.myguest.virtualbox.org>
2024-05-19 13:02:07 +0300

    upd github action and add testing.cpp

---
+++ test.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include <memory>
+#include "ConceptualExample.cpp"
+
+using namespace ConceptualExample;
+
+// Тесты для класса Cow
+TEST(Motivation01Test, CowSee) {
+    Cow cow;
+    EXPECT_EQ(cow.see(), "cow");
+}
+
+TEST(Motivation01Test, CowSay) {
+    Cow cow;
+    EXPECT_EQ(cow.say(), "moo");
+}
+
+// Тесты для класса Pig
+TEST(Motivation01Test, PigSee) {
+    Pig pig;
+    EXPECT_EQ(pig.see(), "pig");
+}
+
+TEST(Motivation01Test, PigSay) {
+    Pig pig;
+    EXPECT_EQ(pig.say(), "oink");
+}
+
+// Тесты для класса Dog
+TEST(Motivation01Test, DogSee) {
+    Dog dog;
+    EXPECT_EQ(dog.see(), "dog");
+}
+
+TEST(Motivation01Test, DogSay) {
+    Dog dog;
+    EXPECT_EQ(dog.say(), "woof");
+}
+
+// Тест для функции seeAndSay
+TEST(Motivation01Test, SeeAndSay) {
+    std::shared_ptr<IAnimal> animal = std::make_shared<Cow>();
+    testing::internal::CaptureStdout();
+    seeAndSay(animal);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "The cow says 'moo' :).\n");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
